dd9b8cecec1147cc8159c38a7ccf3cefa4e00b2e
Bryce Frazier and Sara Hamilton <bryce@>
2018-03-14 11:16:46 -0700

    3 passing tests

--- js/haiku.js
+++ js/haiku.js
@@ -1,19 +1,23 @@
 export class Haiku {
-  constructor(line1, line2,line3) {
+  constructor(line1, line2, line3) {
     this.line1 = line1;
     this.line2 = line2;
     this.line3 = line3;
   }
 
-   vowelCount() {
-    let count = 0;
-    const vowels = ["a","e","i","o","u"];
-    for (let i = 0; i < this.line1.length; i++) {
-      if (vowels.includes(this.line1[i])) {
-        count += 1;
-      }
-    }
-    return count;
+  vowelCount(line) {
+   let count = 0;
+   const vowels = ["a","e","i","o","u"];
+   for (let i = 0; i < line.length; i++) {
+     if (vowels.includes(line[i])) {
+       count += 1;
+     }
+   }
+   return count;
   }
 
+  lineSplit(line) {
+    let splitArray = line.split(" ");
+    return splitArray;
+  }
 }
--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -7,9 +7,20 @@ describe('Haiku', function() {
     reusableHaiku = new Haiku("old pond", "frog leaps in", "water's sound");
   });
 
-  it('will return number of vowels in a word', function() {
-    let result = reusableHaiku.vowelCount();
-    expect(result).toEqual(2);
+  it('will return number of vowels in input', function() {
+    let result = reusableHaiku.vowelCount("testing string");
+    expect(result).toEqual(3);
+  });
+
+  it('will return number of vowels in line3', function() {
+    let result = reusableHaiku.vowelCount(reusableHaiku.line3);
+    expect(result).toEqual(4);
+  });
+
+  it('will return an array of words', function() {
+    let splitArray = reusableHaiku.lineSplit(reusableHaiku.line2);
+    console.log(splitArray);
+    expect(splitArray).toEqual(['frog','leaps','in']);
   });
 
 });
